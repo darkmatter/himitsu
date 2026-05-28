141900b9e3ef9c44305322602d86755da0c8e9b9
Cooper Maruyama <demo@himitsu.dev>
2026-05-27 20:00:37 -0700

    refactor(config): transplant env_dsl → outputs/dsl

---
+++ rust/src/lib.rs
@@ -0,0 +1,15 @@
+#![allow(dead_code, deprecated)]
+
+mod build_info;
+mod cli;
+pub mod completions_cache;
+pub mod config;
+pub mod crypto;
+pub mod error;
+pub mod git;
+pub mod keyring;
+pub mod proto;
+pub mod reference;
+pub mod remote;
+pub mod suggest;
+pub mod tui;
